bba7b46f625a9645e3aaca0e503f4a6b8b9a92ae
LDMtz <leonciodanielmartinez@gmail.com>
2025-12-10 06:10:28 -0700

    Proyecto: Inicializacion - Frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <body className="bg-slate-800">
+        <h1 className="text-green-400">Prueba</h1>
+      </body>
+    </>
+  )
+}
+
+export default App
