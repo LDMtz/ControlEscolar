708e97365e60b059bdd8a4e78dc1088259d52a97
LDMtz <leonciodanielmartinez@gmail.com>
2025-12-12 04:35:21 -0700

    Refactor: Cambió el punto de entrada a la App y su logica

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,25 +4,60 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 
+import type { Application } from 'express';
+
 import db from "./config/database.js";
 
 dotenv.config(); 
 
-const PORT = process.env.PORT || 3000;
+//Clase Server, que inicializa y configura el servidor
+class Server {
+    //Campos de clase
+    private app: Application;
+    private port: string;
 
-const app = express();
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || '3000'; 
 
-//Middlewares
-app.use(cors());
-app.use(helmet());
-app.use(express.json());
-app.use(morgan('dev')); 
+        // 1. Conexión a Base de Datos
+        this.dbConnection();
 
-app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}\n`));
+        // 2. Middlewares
+        this.middlewares();
 
-//Conexion a la BD
-try {
-    await db.authenticate().then(() => console.log("Conexión a la BD establecida."));
-} catch (error) {
-    console.error("Error al conectar a la BD:", error), process.exit(1);
+        //TODO: 3. Definición de Rutas (Endpoints)
+
+        //TODO: 4. Middlewares de Manejo de Errores
+    }
+
+    // Conexión a la BD
+    private async dbConnection() {
+        try {
+            await db.authenticate().then(() => console.log("Conexión a la BD establecida."));
+        } catch (error) {
+            console.error("Error al conectar a la BD:", error), process.exit(1);
+        }
+    }
+
+    //Aplica los middlewares
+    private middlewares() {
+        // Cors: Habilita el acceso desde el front-end
+        this.app.use(cors());
+
+        // Helmet: Cabeceras de seguridad HTTP
+        this.app.use(helmet());
+
+        // Lectura del body: Parseo de JSON
+        this.app.use(express.json());
+
+        // Morgan: Logs de peticiones en consola
+        this.app.use(morgan('dev'));
+    }
+
+    public listen() {
+        this.app.listen(this.port, () => console.log(`Servidor corriendo en http://localhost:${this.port}/api/\n`));
+    }
 }
+
+export default Server;
