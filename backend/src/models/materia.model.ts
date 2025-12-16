4eeb0e305be6eb0d558fb0e7e2e179fce0546314
LDMtz <leonciodanielmartinez@gmail.com>
2025-12-16 05:41:59 -0700

    Model: Se creó modelo Materia

---
+++ backend/src/models/materia.model.ts
@@ -0,0 +1,43 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from './index.js';
+import { addAbortSignal } from 'stream';
+
+export class Materia extends Model {
+  declare id: number;
+  declare codigo?: string;
+  declare nombre: string;
+  declare descripcion?: string;
+  declare created_at: Date;
+  declare updated_at: Date;
+  declare deleted_at?: Date;
+}
+
+Materia.init(
+    {
+        id: { 
+            type: DataTypes.INTEGER, 
+            primaryKey: true, 
+            allowNull: false,
+            autoIncrement: true 
+        },
+        codigo: { 
+            type: DataTypes.STRING(50), 
+            allowNull: true,
+            unique: true 
+        },
+        nombre: { 
+            type: DataTypes.STRING(150), 
+            allowNull: false 
+        },
+        descripcion: { 
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+    },
+    {
+        sequelize,
+        tableName: 'materias',
+        paranoid: true, // soft delete
+        underscored: true,
+    }
+);
