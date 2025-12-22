6e4bf26fe23561e4da5e111afa0f968fb0310d67
LDMtz <leonciodanielmartinez@gmail.com>
2025-12-22 03:15:53 -0700

    Config: Actualizacion de dependencias y config de prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
-
 function App() {
-
   return (
     <>
       <body className="bg-slate-800">
         <h1 className="text-green-400">Prueba</h1>
       </body>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
